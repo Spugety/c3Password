50628c8a9c543eefa49f8a107fb06cd954527f22
Spugety <103894899+Spugety@users.noreply.github.com>
2022-11-21 17:44:27 -0500

    defined generatePassword function, returns random password

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Password Generator Starter Code
 JavaScript Challenge: Password Generator
+This is supposed to generate a random password for the user.
 Trevor Dent
--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Assignment code here
+function generatePassword() {
+  console.log('Button clicked');
 
+
+  return "random password";
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
